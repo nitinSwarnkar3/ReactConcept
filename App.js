497000a57734cd6041b13b23571e40b1f73a7205
nitinSwarnkar <nitinswarnkar332000@gmail.com>
2025-04-22 12:17:57 +0530

    chapter-01

---
+++ App.js
@@ -0,0 +1,32 @@
+
+
+
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement(
+            "h1",
+            { id: "heading" },
+            "h1 tag"
+        ), React.createElement(
+            "h2",
+            { id: "heading" },
+            "h2 tag"
+        )]
+    )
+
+)
+
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World from React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
